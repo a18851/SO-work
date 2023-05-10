3f9d228d832bdc589b9a092639ee5d16fa02e54d
a18851 <98275523+a18851@users.noreply.github.com>
2023-05-10 21:54:13 +0100

    1,2

--- Parte 1/d/contar.c
+++ Parte 1/d/contar.c
@@ -1,29 +1,50 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
-#define TAMANHO 50
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main() {
-char nome[1000];
-int ficheiro, leitura;
-char conteudo[TAMANHO], ch;
-int x=0, i;
+#define BUFFER_SIZE 1024
 
-printf ("Introduza o nome do arquivo : ");
-gets (nome);
+int conta_ficheiro(char* nome_ficheiro) {
+    int fd = open(nome_ficheiro, O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Erro ao abrir ficheiro %s\n", nome_ficheiro);
+        exit(1);
+    }
 
-ficheiro = open(nome, O_RDONLY);
-if (ficheiro == -1){
-  perror("Erro ao abrir");
-  return (1);
+    char buffer[BUFFER_SIZE];
+    int num_linhas = 1;
+    int num_bytes_lidos = 0;
+    int i;
+
+    while ((num_bytes_lidos = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        for (i = 0; i < num_bytes_lidos; i++) {
+            if (buffer[i] == '\n') {
+                num_linhas++;
+            }
+        }
+    }
+
+    if (num_bytes_lidos == -1) {
+        fprintf(stderr, "Erro ao ler ficheiro %s\n", nome_ficheiro);
+        exit(1);
+    }
+
+    close(fd);
+
+    return num_linhas;
 }
-for(i=0;i<TAMANHO;i++){
-leitura = read(ficheiro, conteudo, sizeof(conteudo));
-if(conteudo[i] == '\n') x++;
-}
 
-printf ("\nO ficheiro possui %d linhas \n", x);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Sintaxe inválida. Utilize: conta_ficheiro <nome_do_ficheiro>\n");
+        exit(1);
+    }
 
-close(ficheiro);
-return 0;
+    char* nome_ficheiro = argv[1];
+    int num_linhas = conta_ficheiro(nome_ficheiro);
+
+    printf("O ficheiro %s tem %d linhas\n", nome_ficheiro, num_linhas);
+
+    return 0;
 }
--- Parte 1/f/informa.c
+++ Parte 1/f/informa.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
 #include <time.h>
-#include <string.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Utilização: infoficheiro <nome_do_ficheiro>\n");
+        exit(1);
+    }
+    
+    struct stat info;
+    if (stat(argv[1], &info) != 0) {
+        fprintf(stderr, "Erro ao obter informação do ficheiro.\n");
+        exit(1);
+    }
 
-  char nome[50], atime[50], mtime[50], ctime[50];
-  struct stat buf;
+    char *tipo_ficheiro;
+    switch (info.st_mode & S_IFMT) {
+        case S_IFREG:
+            tipo_ficheiro = "ficheiro normal";
+            break;
+        case S_IFDIR:
+            tipo_ficheiro = "diretoria";
+            break;
+        case S_IFLNK:
+            tipo_ficheiro = "link simbólico";
+            break;
+        default:
+            tipo_ficheiro = "tipo desconhecido";
+            break;
+    }
 
-  stat("teste.txt", &buf);
-  printf("i-node= %d\n", buf.st_ino);
-  //strcpy(st_uid, nome)
-  printf("nome do utilizador dono: %d\n", buf.st_uid);
-  strcpy(mtime, time(&buf.st_mtime));
-  strcpy(atime, time(&buf.st_atime));
-  strcpy(ctime, time(&buf.st_ctime));
+    struct passwd *pw = getpwuid(info.st_uid);
+    struct group  *gr = getgrgid(info.st_gid);
 
-  printf("ultimo acesso: %d\n", atime);
-  printf("ultima modificacao: %d\n", mtime);
-  printf("data de criacao= %d\n", ctime);
-  return 0;
+    printf("Tipo de ficheiro: %s\n", tipo_ficheiro);
+    printf("Número do i-node: %lu\n", info.st_ino);
+    printf("Utilizador dono: %s (%d)\n", pw->pw_name, info.st_uid);
+    printf("Grupo dono: %s (%d)\n", gr->gr_name, info.st_gid);
+    printf("Data de criação: %s", ctime(&info.st_ctime));
+    printf("Data de modificação: %s", ctime(&info.st_mtime));
+    printf("Data de acesso: %s", ctime(&info.st_atime));
+
+    return 0;
 }
+
